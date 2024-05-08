81f3cebe20eef15fa543566a370182b53a159fcd
Félix MARQUET <fpmovie4@gmail.com>
2024-05-08 15:04:46 +0200

    Add SIGINT and SIGTERM handle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "MyTCPClient.h"
+#include <atomic>
+#include <csignal>
+
+std::atomic<bool> shouldStop = false;
+
+void signalHandler( int signum ) {
+    shouldStop = true;
+}
 
 int main(int argc, char* argv[]) {
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);
     int port = 8080;
     if (argc > 1) {
         port = std::stoi(argv[1]);
@@ -8,12 +18,18 @@ int main(int argc, char* argv[]) {
 
     MyTCPClient client("127.0.0.1", port);
 
-    client.start();
+    try{
+        client.start();
 
-    client.sendMessage("servo_moteur;strat;ready;1");
+        while(!client.shouldStop()){
+            usleep(100'000);
+        }
 
-    while (!client.shouldStop()) {
-        usleep(100'000);
+        client.sendMessage("servo_moteur;strat;ready;1");
+    }
+    catch (const std::exception& e){
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
 
     return 0;
